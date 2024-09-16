01b7918c1e3f31cb036d433512527064e136ec92
wanlanli <wanlanli.ky@gmail.com>
2024-09-16 16:30:19 +0200

    new func: add tracking movie visualization

--- cellmate/visualize/__init__.py
+++ cellmate/visualize/__init__.py
@@ -1,10 +1,11 @@
-from ._animation import animate_images
+from ._animation import animate_images, animate_tracking_with_annotation
 from ._utils import label2rgb
 from ._colormap import COLORMAP
 
 
 __all__ = [
     "animate_images",
+    "animate_tracking_with_annotation",
     "label2rgb",
     "COLORMAP",
 ]
--- cellmate/visualize/_animation.py
+++ cellmate/visualize/_animation.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
@@ -19,11 +20,61 @@ def animate_images(image):
     ani : matplotlib.animation.ArtistAnimation
         The created animation object.
     """
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_xticks([])
+    ax.set_yticks([])
     ims = []
     for i in range(image.shape[0]):
         im = ax.imshow(image[i], animated=True)
-        ims.append([im])
+        title = ax.text(0.5, 1.05, f'Frame {i+1}', ha="center", va="baseline", 
+                        fontsize=12, transform=ax.transAxes, animated=True)
+        ims.append([im, title])
+    plt.close()
+    ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
+    return ani
+
+
+def animate_tracking_with_annotation(mask, annotation=False):
+    """
+    Create an animation of the image frames.
+
+    This function takes a 3D image array and creates an animation where each frame of the
+    animation corresponds to a slice along the first dimension of the image array.
+
+    Parameters:
+    -----------
+    image : np.array
+        A 3D numpy array with dimensions [Time x Width x Height].
+
+    Returns:
+    --------
+    ani : matplotlib.animation.ArtistAnimation
+        The created animation object.
+    """
+    from ._utils import label2rgb
+    image = label2rgb(mask)
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ims = []
+    for i in range(image.shape[0]):
+        im_i = []
+        im = ax.imshow(image[i], animated=True)
+        title = ax.text(0.5, 1.05, f'Frame {i+1}', ha="center", va="baseline", 
+                        fontsize=12, transform=ax.transAxes, animated=True)
+        im_i += [im, title]
+
+        if annotation:
+            labels = np.unique(mask[i])[1:]
+            for label in labels:
+                x, y = np.where(mask[i] == label)
+                ann = ax.text(y.mean()-5, x.mean(), f'{label}', fontsize=8, c='w', animated=True)
+                im_i.append(ann)
+        ims.append(im_i)
     plt.close()
     ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
     return ani
